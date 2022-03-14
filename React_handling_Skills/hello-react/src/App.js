e16bddca1e2a050cbe0a29fc75a27e5185bff69c
Suhyun An <54002821+ansu7514@users.noreply.github.com>
2022-03-15 00:09:16 +0900

    22.03.14_React JSX 문법 (3)

--- React_handling_Skills/hello-react/src/App.js
+++ React_handling_Skills/hello-react/src/App.js
@@ -1,9 +1,22 @@
 function App() {
   const text = "당신은 어썸한가요😊";
+  const condition = true;
+  const style = {
+    backgroundColor: 'gray',
+    border: '1px solid black',
+    height: Math.round(Math.random() * 300) + 50,
+    width: Math.round(Math.random() * 300) + 50,
+    WebkitTransition: 'all',
+    MozTransition: 'all',
+    msTransition: 'all'
+  }
+
   return (
     <div>
       <h1>리액트 안녕!</h1>
       <h2>{text}</h2>
+      { condition && '보여주세요' }
+      <div style={style}></div>
     </div>
   );
 }
