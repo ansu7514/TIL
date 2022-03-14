df798c8218aa5ce0123769fc3237816cbd999b2b
Suhyun An <54002821+ansu7514@users.noreply.github.com>
2022-03-15 00:24:49 +0900

    22.03.15_React state (2)

--- React_handling_Skills/hello-react/src/MyComponent.js
+++ React_handling_Skills/hello-react/src/MyComponent.js
@@ -3,13 +3,18 @@ import React, { useState } from "react";
 
 const MyComponent = (props) => {
     // state 설정
-    const [number] = useState(0);
+    const [number, setNumber] = useState(0);
 
     return (
         <div>
             <p>안녕하세요, 제 이름은 {props.name}입니다.</p>
             <p>저는 {props.age}살 입니다.</p>
             <p>숫자 : {number}</p>
+            <button onClick={() => {
+                    // 누를 때마다 숫자 1씩 증가
+                    setNumber(number + 1);
+                }
+            }>더하기</button>
         </div>
     )
 }
