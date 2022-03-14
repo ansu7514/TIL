c714108c03751188145e5793e892700884edc1bc
Suhyun An <54002821+ansu7514@users.noreply.github.com>
2022-03-15 00:13:49 +0900

    22.03.15_React props (1)

--- React_handling_Skills/hello-react/src/App.js
+++ React_handling_Skills/hello-react/src/App.js
@@ -1,25 +1,8 @@
-import './App.css';
+import MyComponent from "./MyComponent";
 
 function App() {
-  const text = "당신은 어썸한가요😊";
-  const condition = true;
-  const style = {
-    backgroundColor: 'gray',
-    border: '1px solid black',
-    height: Math.round(Math.random() * 300) + 50,
-    width: Math.round(Math.random() * 300) + 50,
-    WebkitTransition: 'all',
-    MozTransition: 'all',
-    msTransition: 'all'
-  }
-
   return (
-    <div className='my-div'>
-      <h1>리액트 안녕!</h1>
-      <h2>{text}</h2>
-      { condition && '보여주세요' }
-      <div style={style}></div>
-    </div>
+    <MyComponent/>
   );
 }
 
