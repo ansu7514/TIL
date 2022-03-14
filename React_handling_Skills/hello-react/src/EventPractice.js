67fd1f2ea32fee60f3c6097430f534ba58202324
Suhyun An <54002821+ansu7514@users.noreply.github.com>
2022-03-15 00:34:03 +0900

    22.03.15_React event (2)

--- React_handling_Skills/hello-react/src/EventPractice.js
+++ React_handling_Skills/hello-react/src/EventPractice.js
@@ -7,27 +7,8 @@ const EventPractice = () => {
                 name="message"
                 placeholder="아무거나 입력해보세요"
                 onChange={(e) => {
-                        console.log(e);
-
-                        /*
-                            콘솔에 객체 출력
-                            SyntheticBaseEvent {_reactName: 'onChange', _targetInst: null, type: 'change', nativeEvent: InputEvent, target: input, …}
-                            bubbles: true
-                            cancelable: false
-                            currentTarget: null
-                            defaultPrevented: false
-                            eventPhase: 3
-                            isDefaultPrevented: ƒ functionThatReturnsFalse()
-                            isPropagationStopped: ƒ functionThatReturnsFalse()
-                            isTrusted: true
-                            nativeEvent: InputEvent {isTrusted: true, data: '2', isComposing: false, inputType: 'insertText', dataTransfer: null, …}
-                            target: input
-                            timeStamp: 1067.7999999523163
-                            type: "change"
-                            _reactName: "onChange"
-                            _targetInst: null
-                            [[Prototype]]: Object
-                        */
+                        // 입력 변화값 콘솔에 출력
+                        console.log(e.target.value);
                     }
                 }
             ></input>
