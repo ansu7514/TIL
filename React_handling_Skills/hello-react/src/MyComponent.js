0bda69dd0d21588f4273b3e20ab3ef0c0ee46d32
Suhyun An <54002821+ansu7514@users.noreply.github.com>
2022-03-15 00:23:07 +0900

    22.03.15_React state (1)

--- React_handling_Skills/hello-react/src/MyComponent.js
+++ React_handling_Skills/hello-react/src/MyComponent.js
@@ -1,10 +1,15 @@
 import PropType from 'prop-types';
+import React, { useState } from "react";
 
 const MyComponent = (props) => {
+    // state 설정
+    const [number] = useState(0);
+
     return (
         <div>
             <p>안녕하세요, 제 이름은 {props.name}입니다.</p>
             <p>저는 {props.age}살 입니다.</p>
+            <p>숫자 : {number}</p>
         </div>
     )
 }
