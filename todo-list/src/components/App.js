27c3008fdeeafe3fd19a9d400e65ea4fe8491fc2
Suhyun An <54002821+ansu7514@users.noreply.github.com>
2022-03-21 22:15:05 +0900

    22.03.21_최적화 데이터 준비

--- todo-list/src/components/App.js
+++ todo-list/src/components/App.js
@@ -3,14 +3,19 @@ import { PageTemplate } from './PageTemplate';
 import { TodoInput } from './TodoInput';
 import { TodoList } from './TodoList';
 
+const initialTodos = new Array(500).fill(0).map(
+    (foo, index) => ({
+        id: index,
+        text: `일정 ${index}`,
+        done: false
+    })
+);
+
 const App = (e) => {
     const [state, setState] = useState({
         input: '',  // input 값
         // 일정 데이터 초기값
-        todos: [
-            { id: 0, text: '리액트 공부하기', done: true },
-            { id: 1, text: '컴포넌트 스타일링 해보기', done: false }
-        ]
+        todos: initialTodos,
     });
     const { input, todos } = state;
 
