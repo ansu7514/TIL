788c2405b1fcfc4e6eb0c468305c4c14cc3e7bdf
Suhyun An <54002821+ansu7514@users.noreply.github.com>
2022-03-22 22:32:00 +0900

    22.03.22_react redux action 생성

---
+++ redux-counter/src/actions/index.js
@@ -0,0 +1,20 @@
+/*
+    action 객체를 만드는 액션 생성 함수들을 선언한다. (action creators)
+    여기에서 () => ({})은 function() { return {} }와 동일한 의미이다.
+*/
+
+import * as types from './ActionTyoes';
+
+export const increment = () => ({
+    type: types.INCREMENT
+});
+
+export const decrement = () => ({
+    type: types.DECREMENT
+});
+
+// 다른 액션 생성자들과 달리 파라미터를 가지고 있음
+export const setColor = (color) => ({
+    type: types.SET_COLOR,
+    color
+});
