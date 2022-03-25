78a33416c0d35079282583616cb6a6104f2c4e43
Suhyun An <54002821+ansu7514@users.noreply.github.com>
2022-03-25 22:11:46 +0900

    22.03.25_Hooks useRef (2)
    useRef sample code

---
+++ React_handling_Skills/hooks-tutorial/src/RefSample.js
@@ -0,0 +1,17 @@
+const RefSample = () => {
+    const id = useRef(1);
+    const setId = (n) => {
+        id.current = n;
+    }
+    const printId = () => {
+        console.log(id.current);
+    }
+
+    return (
+        <div>
+            refsample
+        </div>
+    );
+};
+
+export default RefSample;
