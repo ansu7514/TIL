156ddbf57c1f2293e2bd0b6b7cd8a6abf665a00a
Suhyun An <54002821+ansu7514@users.noreply.github.com>
2022-03-30 22:12:26 +0900

    22.03.30_immer 사용하기 (3)
    App 컴포넌트에 immer 적용하기

--- React_handling_Skills/immer-tutorial/src/App.js
+++ React_handling_Skills/immer-tutorial/src/App.js
@@ -1,5 +1,5 @@
-import './App.css';
 import { useCallback, useRef, useState } from 'react';
+import produce from 'immer';
 
 function App() {
   const nextId = useRef(1);
@@ -12,10 +12,11 @@ function App() {
   // input 수정을 위한 함수
   const onChange = useCallback((e) => {
     const { name, value } = e.target;
-    setForm({
-      ...form,
-      [name]: [value]
-    });
+    setForm(
+      produce(form, draft => {
+        draft[name] = value;
+      })
+    );
   }, [form]);
 
   // form 등록을 위한 함수
@@ -28,10 +29,11 @@ function App() {
     };
 
     // array에 새 항목 등록
-    setData({
-      ...data,
-      array: data.array.concat(info)
-    });
+    setData(
+      produce(data, draft => {
+        draft.array.push(info);
+      })
+    );
 
     // form 초기화
     setForm({
@@ -43,10 +45,11 @@ function App() {
 
   // 항목을 삭제하는 함수
   const onRemove = useCallback((id) => {
-    setData({
-      ...data,
-      array: data.array.filter(info => info.id !== id)
-    });
+    setData(
+      produce(data, draft => {
+        draft.array.splice(draft.array.findIndex(info => info.id === id), 1);
+      })
+    );
   }, [data]);
 
   return (
