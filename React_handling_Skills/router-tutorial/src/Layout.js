940e236b19d5d31de71a288bc493bd15a858ce1b
Suhyun An <54002821+ansu7514@users.noreply.github.com>
2022-03-31 22:27:47 +0900

    22.03.31_리액트 라우터로 SPA 개발하기 (12)
    useNavigate

--- React_handling_Skills/router-tutorial/src/Layout.js
+++ React_handling_Skills/router-tutorial/src/Layout.js
@@ -1,10 +1,24 @@
-import { Outlet } from "react-router-dom"
+import { Outlet, useNavigate } from "react-router-dom"
 
 const Layout = () => {
+    const navigate = useNavigate();
+
+    const goBack = () => {
+        // 이전 페이지로 이동
+        navigate(-1);
+    };
+
+    const goArticles = () => {
+        // articles 경로로 이동
+        // replace할 경우 페이지 기록이 남지 않음
+        navigate('/articles', { replace: true });
+    };
+
     return (
         <div>
             <header style={{ background: 'lightgray', padding: 16, fontSize: 24 }}>
-                Header
+                <button onClick={goBack}>뒤로가기</button>
+                <button onClick={goArticles}>게시글 목록</button>
             </header>
             <main>
                 <Outlet />
