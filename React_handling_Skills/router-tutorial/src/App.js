851d35acfcccbdcf93cfdb930ce241178571ba39
Suhyun An <54002821+ansu7514@users.noreply.github.com>
2022-03-31 22:38:51 +0900

    22.03.31_리액트 라우터로 SPA 개발하기 (14)
    NotFound 페이지 만들기

--- React_handling_Skills/router-tutorial/src/App.js
+++ React_handling_Skills/router-tutorial/src/App.js
@@ -4,6 +4,7 @@ import About from './pages/About';
 import Article from "./pages/Article";
 import Articles from "./pages/Articles";
 import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 import Profile from './pages/Profile';
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
       <Route path="/articles" element={<Articles />}>
         <Route path=":id" element={<Article />} />
       </Route>
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
