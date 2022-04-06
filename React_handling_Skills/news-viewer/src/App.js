ce1cd1041942fffe0374e0099090fd59c76c54c1
Suhyun An <54002821+ansu7514@users.noreply.github.com>
2022-04-06 21:14:25 +0900

    22.04.06_뉴스 뷰어 만들기 (4)
    newsapi API 키 발급하기

--- React_handling_Skills/news-viewer/src/App.js
+++ React_handling_Skills/news-viewer/src/App.js
@@ -5,7 +5,7 @@ const App = () => {
   const [data, setData] = useState(null);
   const onClick = async () => {
     try {
-      const response = await axios.get('https://jsonplaceholder.typicode.com/todos/1',);
+      const response = await axios.get('https://newsapi.org/v2/top-headlines?country=kr&apiKey=a4555d33885a4edebc443be06fc8dbbc',);
       setData(response.data);
     } catch (e) {
       console.log(e);
