4427138696a610948fb65de07b5d4c191c1f01a2
Suhyun An <54002821+ansu7514@users.noreply.github.com>
2022-04-08 23:07:51 +0900

    22.04.08_Context API (7)
    색상 선택 컴포넌트 만들기

--- React_handling_Skills/context-tutorial/src/App.js
+++ React_handling_Skills/context-tutorial/src/App.js
@@ -1,10 +1,12 @@
 import ColorBox from "./components/ColorBox";
 import { ColorProvider } from "./contexts/color";
+import SelectColor from "./components/SelectColors";
 
 const App = () => {
   return (
     <ColorProvider>
       <div>
+        <SelectColor />
         <ColorBox />
       </div>
     </ColorProvider>
