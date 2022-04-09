80f3bb259a84f822dbf0c7963ecc0028a4312553
Suhyun An <54002821+ansu7514@users.noreply.github.com>
2022-04-09 22:11:27 +0900

    22.04.09_리덕스 라이브러리 (9)
    render 함수 만들기

--- React_handling_Skills/vanilla-redux/index.js
+++ React_handling_Skills/vanilla-redux/index.js
@@ -43,3 +43,17 @@ function reducer(state = initialState, action) {
 }
 
 const store = createStore(reducer);
+
+const reducer = () => {
+    const state = store.getState(); // 현재 상태를 불러온다.
+    // 토글 처리
+    if (state.toggle) {
+        divToggle.classList.add('active');
+    } else {
+        divToggle.classList.remove('active');
+    }
+    // 카운터 처리
+    counter.innerText = state.counter;
+};
+
+render();
