8c434b34e4aa3c08fa7ee6282e64f0fef5ba43b5
Suhyun An <54002821+ansu7514@users.noreply.github.com>
2022-04-10 19:49:10 +0900

    22.04.10_리액트 애플리케이션 상태 관리하기 (5)
    액션 생성 함수 만들기

--- React_handling_Skills/react-redux-tutorial/src/modules/counter.js
+++ React_handling_Skills/react-redux-tutorial/src/modules/counter.js
@@ -1,2 +1,5 @@
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREATE';
+
+export const increase = () => ({ type: INCREASE });
+export const deincrease = () => ({ type: DECREASE });
