8bd367bc66bbb4d7ef8f132df292876f7def45ea
Suhyun An <54002821+ansu7514@users.noreply.github.com>
2022-04-10 19:51:29 +0900

    22.04.10_리액트 애플리케이션 상태 관리하기 (6)
    초기 상태 및 리듀서 함수 만들기

--- React_handling_Skills/react-redux-tutorial/src/modules/counter.js
+++ React_handling_Skills/react-redux-tutorial/src/modules/counter.js
@@ -3,3 +3,24 @@ const DECREASE = 'counter/DECREATE';
 
 export const increase = () => ({ type: INCREASE });
 export const deincrease = () => ({ type: DECREASE });
+
+const initialState = {
+    number: 0
+};
+
+function counter(state = initialState, action) {
+    switch (action.type) {
+        case INCREASE:
+            return {
+                number: state.number + 1
+            };
+        case DECREASE:
+            return {
+                number: state.number - 1
+            };
+        default:
+            return state;
+    }
+}
+
+export default counter;
