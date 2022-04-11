572afc54a14a32242e3ae616d297cb5e04aa306e
Suhyun An <54002821+ansu7514@users.noreply.github.com>
2022-04-11 22:00:52 +0900

    22.04.11_리액트 애플리케이션 상태 관리하기 (15)
    TodosContainer 만들기

--- React_handling_Skills/react-redux-tutorial/src/App.js
+++ React_handling_Skills/react-redux-tutorial/src/App.js
@@ -1,12 +1,12 @@
-import Todos from "./components/Todo";
 import CounterContainer from "./containers/CounterContainer";
+import TodosContainer from "./containers/TodosContainer";
 
 const App = () => {
   return (
     <div>
       <CounterContainer />
       <hr />
-      <Todos />
+      <TodosContainer />
     </div>
   );
 };
