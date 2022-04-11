2825b6e5e8e3e1b9c6dd497408906dbbf051b13b
Suhyun An <54002821+ansu7514@users.noreply.github.com>
2022-04-11 21:07:02 +0900

    22.04.11_리액트 애플리케이션 상태 관리하기 (14)
    CounterContainer 만들기

--- React_handling_Skills/react-redux-tutorial/src/modules/counter.js
+++ React_handling_Skills/react-redux-tutorial/src/modules/counter.js
@@ -2,7 +2,7 @@ const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREATE';
 
 export const increase = () => ({ type: INCREASE });
-export const deincrease = () => ({ type: DECREASE });
+export const decrease = () => ({ type: DECREASE });
 
 const initialState = {
     number: 0
