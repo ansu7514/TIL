abc6cf7528a6b4b289f8a53da8126378d2b4e8a5
Suhyun An <54002821+ansu7514@users.noreply.github.com>
2022-04-11 20:44:18 +0900

    22.04.11_리액트 애플리케이션 상태 관리하기 (12)
    Provider 컴포넌트를 사용하여 프로젝트에 리덕스 적용하기

--- React_handling_Skills/react-redux-tutorial/src/index.js
+++ React_handling_Skills/react-redux-tutorial/src/index.js
@@ -1,8 +1,14 @@
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
 import rootReducer from './modules';
 
 const store = createStore(rootReducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
