07a46467929e57f6ac483140cdd606166d741cbc
Suhyun An <54002821+ansu7514@users.noreply.github.com>
2022-04-12 23:51:52 +0900

    22.04.12_리액트 애플리케이션 상태 관리하기 (18)
    todos 모듈에 적용하기

--- React_handling_Skills/react-redux-tutorial/src/modules/counter.js
+++ React_handling_Skills/react-redux-tutorial/src/modules/counter.js
@@ -3,8 +3,8 @@ import { createAction, handleActions } from "redux-actions";
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREATE';
 
-export const increase = () => createAction(INCREASE);
-export const decrease = () => createAction(DECREASE);
+export const increase = createAction(INCREASE);
+export const decrease = createAction(DECREASE);
 
 const initialState = {
     number: 0
@@ -16,6 +16,6 @@ const counter = handleActions(
         [DECREASE]: (state, action) => ({ number: state.number - 1 }),
     },
     initialState,
-)
+);
 
 export default counter;
