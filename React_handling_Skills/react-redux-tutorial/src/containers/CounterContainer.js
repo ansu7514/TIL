54b30140a176dc496d1f92f9463722b0ddd47427
Suhyun An <54002821+ansu7514@users.noreply.github.com>
2022-04-13 19:49:30 +0900

    22.04.13_리액트 애플리케이션 상태 관리하기 (21)
    useDispatch를 사용하여 액션 디스패치하기

--- React_handling_Skills/react-redux-tutorial/src/containers/CounterContainer.js
+++ React_handling_Skills/react-redux-tutorial/src/containers/CounterContainer.js
@@ -1,10 +1,15 @@
-import { useSelector } from 'react-redux';
+import { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Counter from '../components/Counter';
+import { increase, decrease } from '../modules/counter';
 
 const CounterContainer = () => {
     const number = useSelector(state => state.counter.number);
+    const dispatch = useDispatch();
+    const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+    const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
     return (
-        <Counter number={number} />
+        <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />
     );
 };
 
