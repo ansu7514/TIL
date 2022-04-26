18785b70634de64e2f2b8cad9227bd3b55c5dc48
Suhyun An <54002821+ansu7514@users.noreply.github.com>
2022-04-26 22:36:53 +0900

    22.04.26_시작 및 회원 인증 구현 (6)
    회원가입 & 로그인 UI 준비하기

---
+++ React_handling_Skills/blog/blog-frontend/src/components/auth/AuthForm.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+/*
+    회원가입 또는 로그인 폼을 보여줌
+*/
+
+const AnthFormBlock = styled.div``;
+
+const AuthForm = () => {
+    return (
+        <AnthFormBlock>
+            AuthForm
+        </AnthFormBlock>
+    );
+};
+
+export default AuthForm;
