c04e3c9bfddb63cbbc7f96f238753f479dce0f95
Suhyun An <54002821+ansu7514@users.noreply.github.com>
2022-04-29 20:00:24 +0900

    22.04.29_글쓰기 기능 구현하기 (3)
    리덕스로 글쓰기 상태 관리하기

--- React_handling_Skills/blog/blog-frontend/src/pages/WritePage.js
+++ React_handling_Skills/blog/blog-frontend/src/pages/WritePage.js
@@ -1,14 +1,14 @@
 import Responsive from "../components/common/Responsive";
-import Editor from "../components/write/Editor";
-import TagBox from "../components/write/TagBox";
-import WriteActionButtons from "../components/write/WriteActionButtons";
+import EditorContainer from "../components/write/EditorContainer";
+import TagBoxContainer from "../components/write/TagBoxContainer";
+import WriteActionButtonsContainer from "../components/write/WrtieActionButtonsContainer";
 
 const WritePage = () => {
     return (
         <Responsive>
-            <Editor />
-            <TagBox />
-            <WriteActionButtons />
+            <EditorContainer />
+            <TagBoxContainer />
+            <WriteActionButtonsContainer />
         </Responsive>
     )
 };
